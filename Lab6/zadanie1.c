554ad227adb97a7902fd97c99f2793d1c0a4a0b0
Szymon Drzewiecki <249478@student.pwr.edu.pl>
2020-12-17 09:48:50 +0100

    lab6 done

--- Lab6/zadanie1.c
+++ Lab6/zadanie1.c
@@ -10,13 +10,26 @@
 int main()
 {
   char userInput[32];
-  int dec1, dec2;
+  int desc1, desc2;
   struct stat status;
   void *adress = NULL;
-  char out_file = "output.txt";
+  char *out_file = "out_file";
 
+  while(1){
+    //pytanie uzytkownika o nazwe pliku
+    printf("Podaj nazwe pliku:");
+    scanf("%s", userInput);
+    desc1 = open(userInput, O_RDONLY);
 
-
-  
+    stat(userInput, &status);
+    desc2 = open(out_file, O_RDWR);
+    truncate(out_file, status.st_size);
+    adress = mmap(NULL, status.st_size, PROT_WRITE | PROT_READ, MAP_SHARED, desc2, 0);
+    read(desc1, adress, status.st_size);
+    msync(adress, status.st_size, MS_SYNC);
+    
+    close(desc1);
+    close(desc2);
+        }
   return 0;
 }
